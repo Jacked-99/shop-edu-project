29dae69a3d00c47538a601ffd6381dfe4b935bd5
Jacked <jaceb134@gmail.com>
2023-02-03 11:54:33 +0100

    better depail display

--- src/Components/PrdouctDetail/ProductDetails.jsx
+++ src/Components/PrdouctDetail/ProductDetails.jsx
@@ -1,12 +1,30 @@
 import Card from "../../UI/BackgroundCard";
-
+import styles from "./ProductDetails.module.scss";
+import { motion } from "framer-motion";
 const ProductDetails = ({ name, desc, img }) => {
   return (
     <Card>
-      <h1>{name}</h1>
-      <div>
-        <p>{desc}</p>
-        <img src={img}></img>
+      <h1 className={styles.name}>{name}</h1>
+      <div className={styles.content}>
+        <motion.p
+          initial={{ y: "-200%", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            duration: 0.7,
+            ease: "easeIn",
+          }}
+        >
+          {desc}
+        </motion.p>
+        <motion.img
+          src={img}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            duration: 1,
+            ease: "easeIn",
+          }}
+        ></motion.img>
       </div>
     </Card>
   );
