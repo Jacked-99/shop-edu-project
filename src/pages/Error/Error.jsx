90bb8afa10482b8d1ff8bb9911928afb69d21d68
Jacked <jaceb134@gmail.com>
2023-04-29 18:14:12 +0200

    Edditet error msg

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
   return (
     <div style={{ display: "block", textAlign: "center" }}>
       <h1>An error had occured 😥</h1>
-      <h2>Sometimes shit just hits the fan</h2>
+      <h2>Sometimes thibgs just go wrong</h2>
       <br></br>
       <p>{error.message}</p>
     </div>
